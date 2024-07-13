942ab1e947299731736d84873c23ede3e76b6939
andrey.karyamin <akaryamin@zerion.io>
2024-07-13 20:06:26 +0200

    models update

--- app/models.go
+++ app/models.go
@@ -10,3 +10,16 @@ type Question struct {
 	Signature string `json:"signature,omitempty" bson:"signature,omitempty"`
 	TokenID   string `json:"tokenID,omitempty" bson:"tokenID,omitempty"`
 }
+
+type SubmitQuestionRequest struct {
+	Address   string `json:"address"`
+	Question  string `json:"question"`
+	Signature string `json:"signature"`
+	Receiver  string `json:"receiver"`
+}
+
+type AnswerQuestionRequest struct {
+	QuestionID string `json:"questionId"`
+	Signature  string `json:"signature"`
+	Answer     string `json:"answer"`
+}

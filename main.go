4eed1240958b8752a618dd99a7e70bb27c71348c
andrey.karyamin <akaryamin@zerion.io>
2024-07-13 12:10:19 +0200

    simple server

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Response struct {
+	Message string `json:"message"`
+}
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	response := Response{Message: "Hello, World!"}
+	json.NewEncoder(w).Encode(response)
+}
+
+func main() {
+	http.HandleFunc("/", helloWorld)
+	http.ListenAndServe(":8080", nil)
+}

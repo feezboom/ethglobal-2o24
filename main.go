0417d681bff7010a8d0044e4667a87374e166522
andrey.karyamin <akaryamin@zerion.io>
2024-07-13 13:16:17 +0200

    add mongodb

--- main.go
+++ main.go
@@ -1,12 +1,46 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Response struct {
 	Message string `json:"message"`
+}
+
+type Entry struct {
+	ID      string `json:"id,omitempty" bson:"id,omitempty"`
+	Content string `json:"content,omitempty" bson:"content,omitempty"`
+}
+
+var client *mongo.Client
+var collection *mongo.Collection
+
+func connectDB() {
+	var err error
+	clientOptions := options.Client().ApplyURI("your-amazon-documentdb-uri")
+
+	client, err = mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+	collection = client.Database("testdb").Collection("entries")
 }
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
@@ -14,7 +48,24 @@ func helloWorld(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(response)
 }
 
+func addEntry(w http.ResponseWriter, r *http.Request) {
+	var entry Entry
+	_ = json.NewDecoder(r.Body).Decode(&entry)
+
+	entry.ID = fmt.Sprintf("%d", time.Now().Unix())
+	_, err := collection.InsertOne(context.TODO(), entry)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(entry)
+}
+
 func main() {
+	connectDB()
+
 	http.HandleFunc("/", helloWorld)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/add", addEntry)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
